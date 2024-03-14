ebf8f6b828c63cb73e371b0b91182925e1859476
Iron-E <code.iron.e@gmail.com>
2024-03-14 01:32:19 -0400

    ref: embed loose files directly

--- home-manager/hosts/_common_/cargo/default.nix
+++ home-manager/hosts/_common_/cargo/default.nix
@@ -1,5 +1,9 @@
 { ... }: {
 	imports = [];
 
-	home.file.".cargo/config.toml".source = ./config.toml;
+	home.file.".cargo/config.toml".text = /* toml */ ''
+		[target.x86_64-unknown-linux-gnu]
+		linker = "clang"
+		rustflags = ["-C", "link-arg=-fuse-ld=/usr/bin/mold"]
+	'';
 }
