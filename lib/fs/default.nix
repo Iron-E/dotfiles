007193c9624ed127507cf96d1890653c719d1de9
Iron-E <code.iron.e@gmail.com>
2024-03-13 23:42:49 -0400

    ref(lib): filter paths directly

--- lib/fs/default.nix
+++ lib/fs/default.nix
@@ -1,29 +1,15 @@
 nixpkgs: # `flake`
 let
-	inherit (nixpkgs) lib;
-
 	entriesToPaths = # convert output of `readDir` to list of path
 	parent: # `path`
 	fsEntries: # the output of `readDir`
 		map (fsEntry: parent + "/${fsEntry}") (builtins.attrNames fsEntries)
 	;
-
-	filterDir = # filter the output of `readDir`
-	pred: # `filterAttrs` predicate
-	path: # `path`
-		lib.filterAttrs pred (builtins.readDir path)
-	;
-
-	filterDirToPaths = # `readDir` the path and then convert the output to a list of paths
-	pred: # `filterAttrs` predicate
-	path: # `path`
-		entriesToPaths path (filterDir pred path)
-	;
 in {
-	inherit entriesToPaths filterDir filterDirToPaths;
+	inherit entriesToPaths;
 
-	readPathsInDir = # `readDir` the path and then convert the output to a list of paths
+	readPaths = # convert output of `readDir` to list of path
 	path: # `path`
-		filterDirToPaths (_: _: true) path
+		entriesToPaths path (builtins.readDir path)
 	;
 }
