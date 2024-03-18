27d9b82c0eba314df29581e28207d8dc03c11b0e
Iron-E <code.iron.e@gmail.com>
2024-03-18 17:08:19 -0400

    feat(_common_): fd

---
+++ home-manager/hosts/_common_/_cli_/fzf/default.nix
@@ -0,0 +1,9 @@
+{ inputs, outputs, config, lib, pkgs, ... }:
+let
+	util = outputs.lib;
+in {
+	imports = util.fs.readSubmodules ./.;
+
+	# home.packages = with pkgs; [ fd ];
+	programs.fzf.enable = true;
+}
