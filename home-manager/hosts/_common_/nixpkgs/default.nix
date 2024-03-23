e07d3c3aac572b904a53f6ee889eb049c9e47149
Iron-E <code.iron.e@gmail.com>
2024-03-23 18:58:52 -0400

    ref: flatten directories

--- home-manager/hosts/_common_/nixpkgs/default.nix
+++ home-manager/hosts/_common_/nixpkgs/default.nix
@@ -4,4 +4,11 @@ let
 	inherit (util.strings) multiline;
 in {
 	imports = util.fs.readSubmodules ./.;
+
+	nixpkgs =
+		outputs.lib.config.nixpkgs
+		(with inputs; [neovim-nightly-overlay nixgl])
+		(with outputs.overlays; [additions modifications])
+		{}
+	;
 }
