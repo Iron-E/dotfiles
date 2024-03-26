a803c07bf234b8f0ba8fcd2cdf2185249f9ec582
Iron-E <code.iron.e@gmail.com>
2024-03-25 23:49:45 -0400

    style(xinit): unnest `home`

--- home-manager/hosts/_extras_/programs+services/gui/xsession/xinit/default.nix
+++ home-manager/hosts/_extras_/programs+services/gui/xsession/xinit/default.nix
@@ -5,13 +5,11 @@ let
 in {
 	imports = util.fs.readSubmodules ./.;
 
-	home = {
-		activation.linkXsessionToXinitrc = # link xsession with xinit so that startx works
-		let
-			homeDir = config.home.homeDirectory;
-			scriptPath = config.xsession.scriptPath;
-		in lib.hm.dag.entryAfter ["writeBoundary"] (multiline /* sh */ ''
-			run ln -sf ${homeDir}/${scriptPath} ${homeDir}/.xinitrc
-		'');
-	};
+	home.activation.linkXsessionToXinitrc = # link xsession with xinit so that startx works
+	let
+		homeDir = config.home.homeDirectory;
+		scriptPath = config.xsession.scriptPath;
+	in lib.hm.dag.entryAfter ["writeBoundary"] (multiline /* sh */ ''
+		run ln -sf ${homeDir}/${scriptPath} ${homeDir}/.xinitrc
+	'');
 }
