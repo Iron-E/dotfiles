e53a9cd512a30cb437b2fc6acb708d9d0a15ec25
Iron-E <code.iron.e@gmail.com>
2024-03-28 14:02:06 -0400

    perf(fish.config.themes): only install active theme

--- home-manager/hosts/_common_/programs+services/shell/fish/config/themes/default.nix
+++ home-manager/hosts/_common_/programs+services/shell/fish/config/themes/default.nix
@@ -6,17 +6,8 @@ let
 in {
 	imports = [];
 
-	xdg.configFile = lib.mapAttrs' (n: lib.nameValuePair "fish/themes/${n}.theme") {
-		highlite = {
-			source = ./highlite.theme;
-			onChange = multiline /* sh */ ''
-				fish_config theme choose highlite
-				fish_config theme save
-			'';
-		};
-	};
+	xdg.configFile."fish/themes/${activeTheme}.theme".source = ./${activeTheme}.theme;
 
-	# activate current theme
 	home.activation.setFishTheme = lib.hm.dag.entryAfter ["writeBoundary"] (multiline /* sh */ ''
 		run ${lib.getExe config.programs.fish.package} -c "fish_config theme choose ${activeTheme} && fish_config theme save"
 	'');
