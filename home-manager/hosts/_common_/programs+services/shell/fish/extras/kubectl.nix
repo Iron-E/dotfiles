c67af770b009eaa54f301779c6d5046a2f5208c4
Iron-E <code.iron.e@gmail.com>
2024-04-08 12:14:34 -0400

    feat(fish): `kubectl-proxy-endpoint` helper

---
+++ home-manager/hosts/_common_/programs+services/shell/fish/extras/kubectl.nix
@@ -0,0 +1,56 @@
+{ inputs, outputs, config, lib, pkgs, ... }:
+let
+	util = outputs.lib;
+	inherit (util.strings) multiline;
+in {
+	imports = [];
+
+	programs.fish.functions.kubectl-proxy-endpoint = {
+		description = "Get the url of a kubectl proxy endpoint";
+		wraps = "kubectl get pods";
+		body = multiline /* fish */ ''
+			argparse \
+				(fish_opt -s h -l help) \
+				(fish_opt -s n -l namespace --required-val) \
+				(fish_opt -s o -l host --required-val) \
+				(fish_opt -s p -l port --required-val) \
+				-- $argv
+
+			if [ -n "$_flag_help" ] || [ -z "$argv[1]" ]
+				echo  "
+			Usage: switch.fish [OPTIONS] [USER]@[HOSTNAME] [-- [HOME-MANAGER OPTIONS]]
+
+			Example: switch.fish --dry-run iron-e@origin --show-trace
+
+			Options:
+			  -h, --help       Print help
+			  -n, --namespace  The pod's namespace
+			  -o, --host       The proxy host
+			  -p, --port       The pod's endpoint port
+			" return
+			end
+
+			if [ -n "$_flag_host" ]
+				set -f host $_flag_host
+			else
+				set -f host "http://localhost:8001"
+			end
+
+			if [ -n "$_flag_namespace" ]
+				set -f namespace $_flag_namespace
+			else
+				set -f namespace default
+			end
+
+			if [ -n "$_flag_port" ]
+				set -f port $_flag_port
+			else
+				set -f port 8080
+			end
+
+			for arg in $argv
+				echo $host/api/v1/namespaces/$namespace/pods/$arg:$port/proxy/
+			end
+		'';
+	};
+}
