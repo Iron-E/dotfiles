f7724cfcbce4939a579fd335101eb023f2b01c13
Iron-E <code.iron.e@gmail.com>
2024-04-08 14:53:10 -0400

    feat: alias `ip` to `ip --color`

--- home-manager/hosts/_common_/home/shell.nix
+++ home-manager/hosts/_common_/home/shell.nix
@@ -8,18 +8,14 @@ in {
 		# some terminals don't clear history correctly
 		clr = "clear && clear";
 
+		# iproute2
+		ip = "ip --color";
+
 		# ls
 		l = "ls -l";
 		la = "l -A";
 
 		# mkdir
 		mkdir = "mkdir -p";
-
-		# nmcli
-		nc = "nmcli -p";
-
-		# xclip
-		P = "xclip -o -selection clipboard";
-		Y = "xclip -i -selection clipboard";
 	};
 }
