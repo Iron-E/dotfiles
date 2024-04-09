9759c65fde8849e0adb4ef3ec7e23c6dd195c0ca
Iron-E <code.iron.e@gmail.com>
2024-04-09 13:30:26 -0400

    feat(neovim): yamlls kubernetes schema

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugin/lsp/lspconfig.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugin/lsp/lspconfig.lua
@@ -74,7 +74,6 @@ setup 'csharp_ls'
 setup 'graphql'
 setup 'nil_ls'
 setup 'tailwindcss'
-setup 'yamlls'
 
 setup('bashls', { filetypes = { 'sh', 'zsh' } })
 
@@ -217,6 +216,28 @@ setup('tsserver', {
 			includeInlayPropertyDeclarationTypeHints = true,
 			includeInlayVariableTypeHints = true,
 			includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+		},
+	},
+})
+
+setup('yamlls', {
+	settings = {
+		yaml = {
+			schemas = {
+				['https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.29.3/all.json'] = '*.yaml',
+				['http://json.schemastore.org/github-workflow'] = '.github/workflows/*',
+				['http://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
+				['http://json.schemastore.org/ansible-stable-2.9'] = 'roles/tasks/*.{yml,yaml}',
+				['http://json.schemastore.org/prettierrc'] = '.prettierrc.{yml,yaml}',
+				['http://json.schemastore.org/kustomization'] = 'kustomization.{yml,yaml}',
+				['http://json.schemastore.org/ansible-playbook'] = '*play*.{yml,yaml}',
+				['http://json.schemastore.org/chart'] = 'Chart.{yml,yaml}',
+				['https://json.schemastore.org/dependabot-v2'] = '.github/dependabot.{yml,yaml}',
+				['https://json.schemastore.org/gitlab-ci'] = '*gitlab-ci*.{yml,yaml}',
+				['https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json'] = '*api*.{yml,yaml}',
+				['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = '*docker-compose*.{yml,yaml}',
+				['https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json'] = '*flow*.{yml,yaml}',
+			},
 		},
 	},
 })
