e480ec44c0dda67f32e7f47bc3806968bbb11dad
Iron-E <code.iron.e@gmail.com>
2024-05-03 12:34:15 -0400

    fix(qt): update from deprecated option

--- home-manager/hosts/_common_/programs+services/gui/qt/default.nix
+++ home-manager/hosts/_common_/programs+services/gui/qt/default.nix
@@ -5,6 +5,6 @@ in {
 	imports = [];
 
 	qt.enable = true;
-	qt.platformTheme = "gtk3"; # “gtk”, “gtk3”, “gnome”, “lxqt”, “qtct”, “kde”
+	qt.platformTheme.name = "gtk3"; # “gtk”, “gtk3”, “gnome”, “lxqt”, “qtct”, “kde”
 	# qt.style.name = config.gtk.theme.name; # might be needed?
 }
