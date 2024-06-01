63d8a0883332164ddd15f00e019e8d396f31a54f
Iron-E <code.iron.e@gmail.com>
2024-06-01 14:22:15 -0400

    fix(neovim): upgrade to trouble v3

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/outline/trouble.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/outline/trouble.lua
@@ -3,18 +3,18 @@ return {{ 'folke/trouble.nvim',
 	cond = vim.g.man ~= true,
 	keys =
 	{
-		{ '<A-w>d', '<Cmd>TroubleToggle workspace_diagnostics<CR>', desc = 'Toggle trouble.nvim workspace diagnostics', mode = 'n' },
+		{ '<A-w>d', '<Cmd>Trouble diagnostics toggle<CR>', desc = 'Toggle trouble.nvim workspace diagnostics', mode = 'n' },
 		{']D',
-			function() require('trouble').next { skip_groups = true, jump = true } end,
+			function() require('trouble').next() end,
 			desc = 'Jump to the next Trouble entry',
 			mode = 'n',
 		},
 		{'[D',
-			function() require('trouble').previous { skip_groups = true, jump = true } end,
+			function() require('trouble').prev() end,
 			desc = 'Jump to the previous Trouble entry',
 			mode = 'n',
 		},
-		{ '<A-w>T', '<Cmd>TodoTrouble<CR>', desc = 'Toggle trouble.nvim todos using todo-comments.nvim', mode = 'n' },
+		{ '<A-w>T', '<Cmd>Trouble todo toggle<CR>', desc = 'Toggle trouble.nvim todos using todo-comments.nvim', mode = 'n' },
 	},
 	opts = function(_, o)
 		o.auto_preview = false
