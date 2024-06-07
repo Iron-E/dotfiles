c799215cdc153b30a38bcebc6b36b732f796b19e
Iron-E <code.iron.e@gmail.com>
2024-06-07 11:49:23 -0400

    feat(neovim): more neotest adapters

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/outline/neotest.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/outline/neotest.lua
@@ -1,14 +1,24 @@
 return {{ 'nvim-neotest/neotest',
 	dependencies = {
+		-- pre-reqs
 		'antoinemadec/FixCursorHold.nvim',
 		'nvim-lua/plenary.nvim',
 		'nvim-neotest/nvim-nio',
-		'nvim-treesitter/nvim-treesitter'
+		'nvim-treesitter/nvim-treesitter',
+
+		-- adapters
+		'Issafalcon/neotest-dotnet',
+		'marilari88/neotest-vitest',
+		'nvim-neotest/neotest-go',
 	},
 	cmd = 'Neotest',
 	keys = {{ '<A-w>t', '<Cmd>Neotest summary<CR>', desc = 'Open test panel', mode = 'n' }},
 	opts = function(_, o)
-		o.adapters = { require 'neotest-dotnet' }
 		o.loglevel = vim.log.levels.OFF
+		o.adapters = {
+			require 'neotest-dotnet',
+			require 'neotest-go',
+			require 'neotest-vitest',
+		}
 	end,
 }}
