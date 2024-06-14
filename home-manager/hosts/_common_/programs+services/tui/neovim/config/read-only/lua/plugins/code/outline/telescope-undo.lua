c323458ae8967d4cdfa1526ecdb183f96e54114c
Iron-E <code.iron.e@gmail.com>
2024-06-14 16:31:17 -0400

    feat(neovim): telescope emulators plugin

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/outline/telescope-undo.lua
+++
@@ -1,5 +0,0 @@
-return {{ 'debugloop/telescope-undo.nvim',
-	config = function() require('telescope').load_extension 'undo' end,
-	dependencies = 'telescope.nvim',
-	keys = {{ '<A-w>u', '<Cmd>Telescope undo<CR>', desc = 'Telescope undo', mode = 'n' }},
-}}
