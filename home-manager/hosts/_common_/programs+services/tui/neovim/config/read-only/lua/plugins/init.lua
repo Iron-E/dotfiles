3b48d8d9ae560f7781647b50734608b1089e7b2e
Iron-E <code.iron.e@gmail.com>
2024-06-25 11:54:47 -0400

    fix(neovim): lazy v11

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/init.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/init.lua
@@ -2,6 +2,7 @@ return {
 	{ 'folke/lazy.nvim', tag = 'stable' },
 	{ import = 'plugins.code' },
 	{ import = 'plugins.input' },
+	{ import = 'plugins.misc' },
 	{ import = 'plugins.theme' },
 	{ import = 'plugins.ui' },
 }
