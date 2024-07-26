1ea6f332077799cdb4ca4168d8b47443f4d6d1c3
Iron-E <code.iron.e@gmail.com>
2024-07-26 11:26:56 -0400

    feat(neovim): mini.ai — select within whole file
    This makes copying a whole buffer to the clipboard really easy:
    
    ```
    \yag
    ```

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/input/mini.ai.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/input/mini.ai.lua
@@ -1,7 +1,46 @@
-return {{ 'echasnovski/mini.ai', opts = {
-	n_lines = 1000,
-	mappings = {
-		goto_left = '[g',
-		goto_right = ']g',
-	},
-}}}
+--- @class mini.ai.cursor_position
+--- @field line integer
+--- @field col integer
+
+--- @class mini.ai.region
+--- @field to? mini.ai.cursor_position
+--- @field from mini.ai.cursor_position
+
+--- @alias mini.ai.type 'a'|'i'
+
+--- @alias mini.ai.custom_textobject fun(ai_type: mini.ai.type): mini.ai.region
+
+return {{ 'echasnovski/mini.ai', opts = function(_, o)
+		o.n_lines = 1000
+		o.mappings = {
+			goto_left = '[g',
+			goto_right = ']g',
+		}
+
+		o.custom_textobjects = {
+			--- @type mini.ai.custom_textobject
+			g = function(ai_type)
+				local from_line =  1
+				local to_line = vim.api.nvim_buf_line_count(0)
+
+				if ai_type == 'i' then
+					from_line = vim.fn.nextnonblank(from_line)
+					to_line = vim.fn.prevnonblank(to_line)
+
+					if (from_line == 0 and to_line == 0) or (from_line > to_line) then
+						return { from = { line = from_line, col = 1 } }
+					end
+				end
+
+				local to_line_text = vim.api.nvim_buf_get_lines(0, to_line - 1, to_line, false)[1]
+				local to_line_length = vim.api.nvim_strwidth(to_line_text)
+				local to_col = math.max(to_line_length, 1)
+
+				return {
+					from = { line = from_line, col = 1 },
+					to = { line = to_line, col = to_col },
+				}
+			end,
+		}
+	end,
+}}
