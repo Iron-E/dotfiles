623599b4e063f6db93ddd5cf057f6a38f1d45cd1
Iron-E <code.iron.e@gmail.com>
2024-08-20 09:46:28 -0400

    fix(neovim): `mini.git` never `setup` when plugin lazy loaded

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/mini.git.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/mini.git.lua
@@ -1,11 +1,9 @@
 return {{ 'echasnovski/mini-git',
-	keys = {{
-		'<Leader>K',
-		'<Cmd>lua MiniGit.show_at_cursor()<CR>',
-		mode = { 's', 'v' },
-		desc = 'Show Git info at cursor',
-	}},
-	config = function()
+	cmd = 'Git',
+	config = true,
+	keys = {{ '<Leader>K', '<Cmd>lua MiniGit.show_at_cursor()<CR>', mode = 'v', desc = 'Show Git info at cursor' }},
+	main = 'mini.git',
+	opts = function()
 		vim.api.nvim_create_autocmd('FileType', {
 			command = 'setlocal foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr()',
 			pattern = { 'diff', 'git' },
