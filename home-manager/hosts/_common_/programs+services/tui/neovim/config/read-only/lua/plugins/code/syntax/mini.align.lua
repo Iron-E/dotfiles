86011b57ae612552e18c74febba4f622f5ac4e7a
Iron-E <code.iron.e@gmail.com>
2024-09-04 09:02:24 -0400

    feat(neovim): markdown table alignment mapping

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/syntax/mini.align.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/syntax/mini.align.lua
@@ -1,10 +1,27 @@
 --- mode for mapping
 local nx = { 'n', 'x' }
 
+--- the options to use when aligning a markdown table
+local align_markdown_table = 'tm<Space><CR>s<Bar><CR>'
+
 return {{ 'echasnovski/mini.align',
 	keys = {
 		{ '<Leader>a', mode = nx, desc = 'Align' },
 		{ '<Leader>A', mode = nx, desc = 'Align with preview' },
+		{ '<Leader>t', '<Leader>aip' .. align_markdown_table,
+			ft = 'markdown',
+			mode = 'n',
+			remap = true,
+			silent = true,
+			desc = 'Align markdown table in paragraph',
+		},
+		{ '<Leader>t', '<Leader>a' .. align_markdown_table,
+			ft = 'markdown',
+			mode = 'x',
+			remap = true,
+			silent = true,
+			desc = 'Align selected markdown table',
+		},
 	},
 	opts = function(_, o)
 		o.mappings = {
