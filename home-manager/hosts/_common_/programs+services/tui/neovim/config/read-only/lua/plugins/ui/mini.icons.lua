fa760414585631f7b9f7288da5472c799dece02e
Iron-E <code.iron.e@gmail.com>
2024-12-18 10:10:25 -0500

    fix(neovim): use correct LSP colors with mini.icons
    It seems that they don't default to using the built-in HL groups here…
    I just overrode them myself.

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/ui/mini.icons.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/ui/mini.icons.lua
@@ -5,4 +5,46 @@ return {{ 'echasnovski/mini.icons',
 		icons.setup(opts)
 		icons.mock_nvim_web_devicons()
 	end,
+	opts = function(_, o)
+		o.default = {
+			lsp = { hl = '@markup.raw' },
+		}
+
+		o.lsp = {
+			array = { hl = '@punctuation.bracket' },
+			boolean = { hl = '@boolean' },
+			class = { hl = '@lsp.type.class' },
+			color = { hl = '@label' },
+			constant = { hl = '@constant' },
+			constructor = { hl = '@constructor' },
+			enum = { hl = '@lsp.type.enum' },
+			enummember = { hl = '@lsp.type.enumMember' },
+			event = { hl = '@lsp.type.event' },
+			field = { hl = '@variable.member' },
+			file = { hl = 'Directory' },
+			folder = { hl = 'Directory' },
+			['function'] = { hl = '@function' },
+			interface = { hl = '@lsp.type.interface' },
+			key = { hl = '@property' },
+			keyword = { hl = '@keyword' },
+			method = { hl = '@function.method' },
+			module = { hl = '@module' },
+			namespace = { hl = '@lsp.type.namespace' },
+			null = { hl = '@constant.builtin' },
+			number = { hl = '@number' },
+			object = { hl = '@variable' },
+			operator = { hl = '@operator' },
+			package = { hl = '@module' },
+			property = { hl = '@variable.member' },
+			reference = { hl = '@type.pointer' },
+			snippet = { hl = '@function.macro' },
+			string = { hl = '@string' },
+			struct = { hl = '@structure' },
+			text = { hl = '@markup.quote' },
+			typeparameter = { hl = '@lsp.type.typeParameter' },
+			unit = { hl = '@type.builtin' },
+			value = { hl = '@operator' },
+			variable = { hl = '@variable' },
+		}
+	end,
 }}
