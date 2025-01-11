1e2591b122598ae0d6bac5324027ac0fc718d4d7
Iron-E <code.iron.e@gmail.com>
2025-01-11 15:56:52 -0500

    fix(librewolf): font in browser distorted

--- home-manager/hosts/_common_/programs+services/gui/librewolf/config/overrides/font.nix
+++ home-manager/hosts/_common_/programs+services/gui/librewolf/config/overrides/font.nix
@@ -5,16 +5,16 @@ let
 in {
 	imports = [];
 
-	home.packages = with pkgs.nerd-fonts; [
-		jetbrains-mono # monospace font
+	home.packages = with pkgs; [
+		nerd-fonts.jetbrains-mono # monospace font
 		open-dyslexic # (sans) serif font
 	];
 
 	programs.librewolf.settings = lib.mapAttrs' (n: lib.nameValuePair "font.${n}") {
 		"minimum-size.x-western" = 18;
 		"name.monospace.x-western" = "JetBrainsMono Nerd Font Mono";
-		"name.sans-serif.x-western" = "OpenDyslexic Nerd Font";
-		"name.serif.x-western" = "OpenDyslexic Nerd Font";
+		"name.sans-serif.x-western" = "OpenDyslexic";
+		"name.serif.x-western" = "OpenDyslexic";
 		"size.monospace.x-western" = 14;
 		"size.variable.x-western" = 18;
 	};
