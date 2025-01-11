3c2194e3e2a2b7ed83d7d975ce3987a54c9a21bc
Iron-E <code.iron.e@gmail.com>
2025-01-11 16:17:29 -0500

    fix(neovim): error when viewing scissors snippets

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/completion/scissors.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/completion/scissors.lua
@@ -4,8 +4,12 @@ return {{ 'chrisgrieser/nvim-scissors',
 		{'<A-w>s', function() require('scissors').addNewSnippet() end, desc = 'Add snippet with scissors', mode = {'n', 'x'}},
 		{'<A-w>S', function() require('scissors').editSnippet() end, desc = 'Edit snippet with scissors', mode = 'n'},
 	},
+	config = function(_, o)
+		require('scissors').setup(o)
+		require('scissors.config').config.telescope.opts.layout_config.preview_width = nil
+	end,
 	opts = function(_, o)
-		o.jsonFormatter = 'jq'
+		o.jsonFormatter = { 'jq', '--monochrome-output', '--sort-keys', '--tab' }
 		o.editSnippetPopup = {
 			height = 0.85, -- relative to the window, number between 0 and 1
 			width = 0.8,
