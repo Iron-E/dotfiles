f24f3b4b695ce53f7d45d983f5e63968398b62c2
Iron-E <code.iron.e@gmail.com>
2025-02-10 20:53:36 -0500

    feat(common): nerdctl snippets
    Because I use it on linux

--- home-manager/hosts/_common_/programs+services/ctl/docker/shell.nix
+++ home-manager/hosts/_common_/programs+services/ctl/docker/shell.nix
@@ -6,9 +6,13 @@ in {
 
 	home.shellAliases = {
 		d = "docker";
+		de = "docker exec";
 		di = "docker image";
+		dil = "docker image ls";
+		din = "docker image inspect";
+		dir = "docker image rm";
 		dp = "docker pull";
-		dr = "docker run";
+		dr = "docker run --rm -it";
 		dx = "docker buildx build";
 	};
 }
---
+++ home-manager/hosts/_common_/programs+services/ctl/nerdctl/shell.nix
@@ -0,0 +1,18 @@
+{ inputs, outputs, config, lib, pkgs, ... }:
+let
+	util = outputs.lib;
+in {
+	imports = [];
+
+	home.shellAliases = {
+		n = "nerdctl";
+		ne = "nerdctl exec";
+		ni = "nerdctl image";
+		nil = "nerdctl image ls";
+		nin = "nerdctl image inspect";
+		nir = "nerdctl image rm";
+		np = "nerdctl pull";
+		nr = "nerdctl run --rm -it";
+		nx = "nerdctl buildx build";
+	};
+}
