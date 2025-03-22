b589dd70064e493122c04dac7781099b403f25b9
Iron-E <code.iron.e@gmail.com>
2025-03-22 13:33:18 -0400

    feat(kubectl): more aliases
    This will make it easier to execute kubectl commands

--- home-manager/hosts/_extras_/programs+services/ctl/kubectl/shell.nix
+++ home-manager/hosts/_extras_/programs+services/ctl/kubectl/shell.nix
@@ -5,5 +5,40 @@ let
 in {
 	imports = [];
 
-	home.shellAliases.k = "kubectl";
+	home.shellAliases = {
+		k = "kubectl";
+
+		ka = "k apply";
+		kaf = "k apply -f";
+
+		kat = "k attach";
+
+		kau = "k auth";
+		kani = "kau can-i";
+
+		konf = "k config";
+
+		kcp = "k cp";
+
+		kb = "k debug";
+
+		krm = "k delete";
+		krmf = "k delete -f";
+
+		kd = "k diff";
+
+		ki = "k describe";
+
+		kv = "k events";
+
+		kh = "k explain";
+
+		kg = "k get";
+
+		kl = "k logs";
+
+		kr = "k run";
+
+		kt = "k top";
+	};
 }
