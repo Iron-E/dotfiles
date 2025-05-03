29b29b32d38e5ae05961676b9dab8b4a2740aeb9
Iron-E <code.iron.e@gmail.com>
2025-05-03 19:31:35 -0400

    ref(librewolf): use nerd font for serif font

--- home-manager/hosts/_common_/programs+services/gui/librewolf/config/overrides/font.nix
+++ home-manager/hosts/_common_/programs+services/gui/librewolf/config/overrides/font.nix
@@ -2,16 +2,16 @@
 {
   imports = [ ];
 
-  home.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono # monospace font
+  home.packages = with pkgs.nerd-fonts; [
+    jetbrains-mono # monospace font
     open-dyslexic # (sans) serif font
   ];
 
   programs.librewolf.settings = lib.mapAttrs' (n: lib.nameValuePair "font.${n}") {
     "minimum-size.x-western" = 18;
     "name.monospace.x-western" = "JetBrainsMono Nerd Font Mono";
-    "name.sans-serif.x-western" = "OpenDyslexic";
-    "name.serif.x-western" = "OpenDyslexic";
+    "name.sans-serif.x-western" = "OpenDyslexic Nerd Font";
+    "name.serif.x-western" = "OpenDyslexic Nerd Font";
     "size.monospace.x-western" = 14;
     "size.variable.x-western" = 18;
   };
