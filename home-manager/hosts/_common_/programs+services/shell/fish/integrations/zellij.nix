431ad7da74fbef8a93caebb151d9909b54d634e8
Iron-E <code.iron.e@gmail.com>
2025-06-21 15:51:26 -0400

    feat(zellij): get output of command in floating window using fish
    This allows me to re-implement Wezterm's Ctrl+Shift+u keybinding.
    See previous commit.
    
    However, I implemented it in such a way that I will be able to use it
    for all sorts of things; not just this one use case.

--- home-manager/hosts/_common_/programs+services/shell/fish/integrations/zellij.nix
+++ home-manager/hosts/_common_/programs+services/shell/fish/integrations/zellij.nix
@@ -1,11 +1,52 @@
-{ ... }:
+{ config, lib, ... }:
 {
   imports = [ ];
 
-  programs.fish.interactiveShellInit = # fish
-    ''
-      if command -qs zellij
+  programs.fish = lib.optionalAttrs config.programs.zellij.enable {
+    functions = {
+      zc = {
+        description = "Run a command in a floating zellij window and write its stdout";
+        body = # fish
+          ''
+            # setup
+            set -f dir (mktemp -d)
+            set -f pipe $dir/fzf.pipe
+            mkfifo $pipe
+
+            # script
+            zellij run \
+            	--floating \
+              --pinned=true \
+            	--close-on-exit \
+              --name $argv[1] \
+            	-- \
+            	fish \
+            	--command \
+            	"$argv > $pipe"
+
+            cat $pipe
+
+            # cleanup
+            rm -rf $dir
+          '';
+      };
+
+      zcw = {
+        description = "Run a command in a floating zellij window and write the stdout to the current pane";
+        wraps = "zc";
+        body = # fish
+          ''
+            set -f output (zc $argv)
+            if [ -n $output ]
+              zellij action write-chars (zc $argv)
+            end
+          '';
+      };
+    };
+
+    interactiveShellInit = # fish
+      ''
         zellij setup --generate-completion fish | source
-      end
-    '';
+      '';
+  };
 }
