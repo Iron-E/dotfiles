ee1aca8bbc23190674fe9875f0a22aefc402983d
Iron-E <code.iron.e@gmail.com>
2025-07-04 12:27:41 -0400

    fix(neovim): treesitter-context command changed

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/outline/treesitter-context.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/outline/treesitter-context.lua
@@ -2,7 +2,7 @@ return {{ 'nvim-treesitter/nvim-treesitter-context',
 	cmd = 'TSContextToggle',
 	dependencies = 'nvim-treesitter',
 	event = vim.g.lazy_event_file_read,
-	keys = {{ '<A-w>c', '<Cmd>TSContextToggle<CR>', desc = 'Toggle TS context', mode = 'n' }},
+	keys = {{ '<A-w>c', '<Cmd>TSContext toggle<CR>', desc = 'Toggle TS context', mode = 'n' }},
 	opts = function(_, o)
 		o.max_lines = 3
 	end,
