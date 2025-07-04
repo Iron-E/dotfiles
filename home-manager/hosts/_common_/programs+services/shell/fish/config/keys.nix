a9e5369dd9a124f4d25bd8683ff96c04bb9bb232
Iron-E <code.iron.e@gmail.com>
2025-07-04 14:34:04 -0400

    ref(fish): make initialization steps more specific
    The init file held keybinds and greeting.
    When I wrote this, I didn't know that init steps
    were just concated by nix.

---
+++ home-manager/hosts/_common_/programs+services/shell/fish/config/keys.nix
@@ -0,0 +1,39 @@
+{ ... }:
+{
+  imports = [ ];
+
+  programs.fish = {
+    # used below, in the init phase
+    functions.vi_bindings = {
+      description = "vi keybindings with autosuggestion acceptance";
+      body = # fish
+        ''
+          fish_vi_key_bindings
+
+          bind -M insert alt-j down-or-search
+          bind -M insert alt-k up-or-search
+          for mode in default insert visual
+            bind -M $mode ctrl-space forward-char
+            bind -M $mode ctrl-f forward-word
+          end
+
+          if command -q watch || functions -q watch
+            for mode in default insert
+              bind -M $mode alt-w 'fish_commandline_prepend watch'
+            end
+          end
+
+          if command -q bat || functions -q bat
+            for mode in default insert
+              bind -M $mode alt-b 'fish_commandline_append " | bat"'
+            end
+          end
+        '';
+    };
+
+    interactiveShellInit = # fish
+      ''
+        set -g fish_key_bindings vi_bindings # set keybindings
+      '';
+  };
+}
