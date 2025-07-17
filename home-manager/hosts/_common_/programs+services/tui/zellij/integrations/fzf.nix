0c6472aa04983d575070be85e8606afcf6642b89
Iron-E <code.iron.e@gmail.com>
2025-07-17 09:18:37 -0400

    fix(fzf): remove comments from config file
    They are not stripped when loading in via shell keybinds.

--- home-manager/hosts/_common_/programs+services/tui/zellij/integrations/fzf.nix
+++ home-manager/hosts/_common_/programs+services/tui/zellij/integrations/fzf.nix
@@ -2,10 +2,10 @@
 {
   imports = [ ];
 
+  # HACK: the preview doesn't render correctly
+  #       for zellij without refreshing
   home.file.${config.home.sessionVariables.FZF_DEFAULT_OPTS_FILE}.text = # conf
     ''
-      # HACK: the preview doesn't render correctly
-      #       for zellij without refreshing
       --bind='start:toggle-preview+toggle-preview'
     '';
 }
