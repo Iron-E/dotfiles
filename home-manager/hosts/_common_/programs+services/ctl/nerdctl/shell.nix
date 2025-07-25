c72168e68f357c895e5aa717b42851432b3495a7
Iron-E <code.iron.e@gmail.com>
2025-07-25 09:25:59 -0400

    feat(nerdctl): update aliases to match docker

--- home-manager/hosts/_common_/programs+services/ctl/nerdctl/shell.nix
+++ home-manager/hosts/_common_/programs+services/ctl/nerdctl/shell.nix
@@ -4,12 +4,21 @@
 
   home.shellAliases = {
     n = "nerdctl";
+    nc = "nerdctl container";
+    nca = "nerdctl container attach";
+    nck = "nerdctl container kill";
+    ncl = "nerdctl container ls --all";
+    ncn = "nerdctl container prune";
+    nco = "nerdctl container stop";
+    ncp = "nerdctl container cp";
+    ncr = "nerdctl container rm --force";
     ne = "nerdctl exec";
     ni = "nerdctl image";
     nil = "nerdctl image ls";
     nin = "nerdctl image inspect";
+    nip = "nerdctl image pull";
     nir = "nerdctl image rm";
-    np = "nerdctl pull";
+    nis = "nerdctl image save";
     nr = "nerdctl run --rm";
     nx = "nerdctl buildx build";
   };
