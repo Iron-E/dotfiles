a1d082543c7cfdc109faee0a105866e5c71770c4
Iron-E <code.iron.e@gmail.com>
2025-07-25 09:25:21 -0400

    ref(docker): rename alias dp -> dip
    I always think docker image pull in my head.

--- home-manager/hosts/_common_/programs+services/ctl/docker/shell.nix
+++ home-manager/hosts/_common_/programs+services/ctl/docker/shell.nix
@@ -16,9 +16,9 @@
     di = "docker image";
     dil = "docker image ls";
     din = "docker image inspect";
+    dip = "docker image pull";
     dir = "docker image rm";
     dis = "docker image save";
-    dp = "docker pull";
     dr = "docker run --rm";
     dx = "docker buildx build";
   };
