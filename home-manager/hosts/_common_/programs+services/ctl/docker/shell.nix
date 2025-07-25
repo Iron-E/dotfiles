a5dc4b384c1134171e36aae764195bd067c38408
Iron-E <code.iron.e@gmail.com>
2025-07-25 09:17:21 -0400

    feat(docker): alias dis -> docker image save

--- home-manager/hosts/_common_/programs+services/ctl/docker/shell.nix
+++ home-manager/hosts/_common_/programs+services/ctl/docker/shell.nix
@@ -9,6 +9,7 @@
     dil = "docker image ls";
     din = "docker image inspect";
     dir = "docker image rm";
+    dis = "docker image save";
     dp = "docker pull";
     dr = "docker run --rm";
     dx = "docker buildx build";
