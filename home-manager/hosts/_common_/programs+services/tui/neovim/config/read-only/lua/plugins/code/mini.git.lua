3d8b5fa93b8d7d1ba490f94f5cf1522796a97185
Iron-E <code.iron.e@gmail.com>
2025-07-25 12:09:33 -0400

    ref(neovim): allow mini.git info in normal mode

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/mini.git.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/code/mini.git.lua
@@ -1,13 +1,21 @@
 return {{ 'echasnovski/mini-git',
-	cmd = 'Git',
-	config = true,
-	keys = {{ '<Leader>K', '<Cmd>lua MiniGit.show_at_cursor()<CR>', mode = 'v', desc = 'Show Git info at cursor' }},
 	main = 'mini.git',
-	opts = function()
+
+	cmd = 'Git',
+	keys = {{
+		'<Leader>K',
+		'<Cmd>lua MiniGit.show_at_cursor()<CR>',
+		mode = { 'n', 'v' },
+		desc = 'Show Git info at cursor',
+	}},
+
+	init = function()
 		vim.api.nvim_create_autocmd('FileType', {
-			command = 'setlocal foldmethod=expr foldexpr=v:lua.MiniGit.diff_foldexpr()',
+			command = [[setlocal foldmethod=expr foldexpr=v:lua.require('mini.git').diff_foldexpr()]],
 			pattern = { 'diff', 'git' },
 			group = 'config',
 		})
 	end,
+
+	config = true,
 }}
