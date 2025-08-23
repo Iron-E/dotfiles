a95de1025f426b46deb5fe98e3fdca8cfdab33fd
Iron-E <code.iron.e@gmail.com>
2025-08-23 18:36:06 -0400

    feat(git): push branch and set upstream
    One of the most common new-branch operations for me is `gpu origin
    <branch>`
    
    This alias, `g up`, will alleviate the need to remember the current
    branch somewhat.
    
    Closes: #2

--- home-manager/hosts/_common_/programs+services/cli/git/config/alias.nix
+++ home-manager/hosts/_common_/programs+services/cli/git/config/alias.nix
@@ -5,5 +5,9 @@
   programs.git.aliases = {
     graph = "log --graph --pretty=format:'%C(#ffb7b7)%h%C(reset bold) %an%C(reset) %s %C(#ff4090)(%cr)%C(bold #ffa6ff)%d%C(reset)' --abbrev-commit --date=relative";
     main = "symbolic-ref refs/remotes/origin/HEAD --short";
+    up = # sh
+      ''
+        ! git push --set-upstream origin "$(git name-rev --name-only HEAD)"
+      '';
   };
 }
