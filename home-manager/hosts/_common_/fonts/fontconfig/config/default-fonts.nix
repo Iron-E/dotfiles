14c23f4d8306097a45c2b085d671d0be48db1816
Iron-E <code.iron.e@gmail.com>
2025-09-27 11:29:00 -0400

    fix(fontconfig): not installing default fonts unless gtk is enabled
    This was because gtk held the configuration for the fonts.

--- home-manager/hosts/_common_/fonts/fontconfig/config/default-fonts.nix
+++ home-manager/hosts/_common_/fonts/fontconfig/config/default-fonts.nix
@@ -1,15 +1,22 @@
 { pkgs, config, ... }:
 {
-  imports = [ ];
+  imports = [ ../../../programs+services/gui/gtk/config/font.nix ];
 
   home.packages = with pkgs; [
+    config.gtk.font.package
     jetbrains-mono
     nerd-fonts.symbols-only
   ];
 
   fonts.fontconfig.defaultFonts = {
-    monospace = [ "JetBrains Mono" "Symbols Nerd Font Mono" ];
-    serif = [ config.gtk.font.name "Symbols Nerd Font Mono" ];
+    monospace = [
+      "JetBrains Mono"
+      "Symbols Nerd Font Mono"
+    ];
+    serif = [
+      config.gtk.font.name
+      "Symbols Nerd Font Mono"
+    ];
     sansSerif = config.fonts.fontconfig.defaultFonts.serif;
   };
 }
