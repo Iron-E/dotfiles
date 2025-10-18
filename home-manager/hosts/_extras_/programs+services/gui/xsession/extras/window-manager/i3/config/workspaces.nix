c04f1b952a11b2622fe238741fd0341566291079
Iron-E <code.iron.e@gmail.com>
2025-10-17 21:43:14 -0400

    fix(i3): invalid quoting
    It seems the default nix i3 config generation has some odd quoting.
    Maybe it doesn't? Maybe my config is messed up. Either way, I've now
    successfully done string templating of my i3 config.
    
    Pretty cool that can be done here. Wondering if that's totally
    necessary.

--- home-manager/hosts/_extras_/programs+services/gui/xsession/extras/window-manager/i3/config/workspaces.nix
+++ home-manager/hosts/_extras_/programs+services/gui/xsession/extras/window-manager/i3/config/workspaces.nix
@@ -37,70 +37,74 @@ in
 {
   imports = [ ];
 
-  xsession.windowManager.i3.config = {
-    inherit defaultWorkspace;
-    startup = toList (pair "command" "${i3Exe "i3-msg"} 'workspace ${defaultWorkspace}'");
-    workspaceAutoBackAndForth = true;
+  xsession.windowManager.i3 = {
+    config = {
+      inherit defaultWorkspace;
+      startup = toList (pair "command" "${i3Exe "i3-msg"} 'workspace ${defaultWorkspace}'");
+      workspaceAutoBackAndForth = true;
 
-    assigns = {
-      ${workspace 1} = [
-        { class = "Chromium"; }
-        { class = "librewolf"; }
-        { instance = "chromium"; }
-        { instance = "Navigator"; }
-      ];
+      assigns = {
+        "\"${workspace 1}\"" = [
+          { class = "Chromium"; }
+          { class = "librewolf"; }
+          { instance = "chromium"; }
+          { instance = "Navigator"; }
+        ];
 
-      ### Word Processors
-      ${workspace 3} = [
-        { instance = "libreoffice"; }
-      ];
+        ### Word Processors
+        "\"${workspace 3}\"" = [
+          { instance = "libreoffice"; }
+        ];
 
-      ### Chat Applications
-      ${workspace 5} = [
-        { class = "Signal"; }
-      ];
+        ### Chat Applications
+        "\"${workspace 5}\"" = [
+          { class = "Signal"; }
+        ];
+      };
+
+      keybindings =
+        let
+          genWorkspaceMaps =
+            lhsFn: # string -> string
+            rhsFn: # string -> string
+            pipe workspaces [
+              length # ["1 | Foo" "2 | Bar" ...] -> 10
+              (genList (
+                i:
+                let
+                  pipe' = pipe i;
+                  lhs = pipe' [
+                    toString
+                    lhsFn
+                  ];
+
+                  rhs = pipe' [
+                    workspace
+                    (v: ''workspace "${toString v}"'')
+                    rhsFn
+                  ];
+                in
+                nameValuePair lhs rhs
+              )) # -> [{name = lhs; value = rhs;} ...]
+
+              listToAttrs # -> { lhs = rhs; lhs2 = rhs2; ... }
+            ];
+        in
+        util.attrsets.concat [
+          (genWorkspaceMaps lhsMod id)
+          (genWorkspaceMaps lhsModShift (v: "move container to ${v}"))
+
+          ## Move focused workspace to another monitor
+          {
+            ${lhsMod greater} = "move workspace to output right";
+            ${lhsMod less} = "move workspace to output left";
+            ${lhsMod tab} = "workspace next";
+            ${lhsModShift tab} = "workspace prev";
+          }
+        ];
     };
 
-    keybindings =
-      let
-        genWorkspaceMaps =
-          lhsFn: rhsFn:
-          pipe workspaces [
-            length # ["1 | Foo" "2 | Bar" ...] -> 10
-            (genList (
-              i:
-              let
-                pipe' = pipe i;
-              in
-              nameValuePair
-                (pipe' [
-                  toString
-                  lhsFn
-                ])
-                (pipe' [
-                  workspace
-                  (v: "workspace ${toString v}")
-                  rhsFn
-                ])
-            )) # -> [{name = lhs; value = rhs;} ...]
-
-            listToAttrs # -> { lhs = rhs; lhs2 = rhs2; ... }
-          ];
-      in
-      util.attrsets.concat [
-        (genWorkspaceMaps lhsMod id)
-        (genWorkspaceMaps lhsModShift (v: "move container to ${v}"))
-
-        ## Move focused workspace to another monitor
-        {
-          ${lhsMod greater} = "move workspace to output right";
-          ${lhsMod less} = "move workspace to output left";
-          ${lhsMod tab} = "workspace next";
-          ${lhsModShift tab} = "workspace prev";
-        }
-      ];
-
-    workspaceOutputAssign =
+    extraConfig =
       let
         laptop = [ "eDP" ];
 
@@ -117,17 +121,18 @@ in
         output =
           workspace: # string
           outputs: # [string]
-          {
-            inherit workspace;
-            output = concatStringsSep " " (outputs ++ lhsMonitor ++ laptop);
-          };
+          # i3config
+          ''workspace "${workspace}" output ${concatStringsSep " " (outputs ++ lhsMonitor ++ laptop)}'';
+
       in
-      [
-        (output (workspace 1) rhsMonitor)
-        (output (workspace 2) [ ])
-        (output (workspace 3) [ ])
-        (output (workspace 4) rhsMonitor)
-        (output (workspace 5) rhsMonitor)
-      ];
+      # i3config
+      ''
+        ${output (workspace 1) rhsMonitor}
+        ${output (workspace 2) [ ]}
+        ${output (workspace 3) [ ]}
+        ${output (workspace 4) rhsMonitor}
+        ${output (workspace 5) rhsMonitor}
+      '';
   };
+
 }
