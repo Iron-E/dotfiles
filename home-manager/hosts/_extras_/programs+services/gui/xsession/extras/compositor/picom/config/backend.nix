723e835801e0d3f895923067f1776f0ddf43ccda
Iron-E <code.iron.e@gmail.com>
2025-10-18 19:12:48 -0400

    fix(picom): migrate to new config format

--- home-manager/hosts/_extras_/programs+services/gui/xsession/extras/compositor/picom/config/backend.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-{
-  imports = [ ];
-
-  # Backend to use: "xrender" or "glx".
-  # GLX backend is typically much faster but depends on a sane driver.
-  services.picom.backend = "xr_glx_hybrid";
-}
