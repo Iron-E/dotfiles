d56b02ae1529336145571899a81597af62ab3d20
Iron-E <code.iron.e@gmail.com>
2025-10-23 21:39:48 -0400

    feat(sway): configure startup applications
    I noticed that sway does not have `notification = false` support.
    I may have to manually set the startup services in the future if need
    be.

---
+++ home-manager/hosts/_extras_/programs+services/gui/wayland/sway/startup.nix
@@ -0,0 +1,8 @@
+{ config, ... }:
+{
+  imports = [ ];
+
+  wayland.windowManager.sway.config.startup = [
+    { command = config.wayland.windowManager.sway.config.terminal; } # start terminal
+  ];
+}
