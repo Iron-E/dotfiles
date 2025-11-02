e350b3c850c3a9758ce47619e7ffbe2fb2bc2f53
Iron-E <code.iron.e@gmail.com>
2025-11-02 15:57:39 -0500

    feat(fuzzel): search and run executable

--- home-manager/hosts/_extras_/programs+services/gui/wayland/extras/gui/fuzzel/integrations/sway/keys.nix
+++ home-manager/hosts/_extras_/programs+services/gui/wayland/extras/gui/fuzzel/integrations/sway/keys.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [ ../../../../window-manager/sway/lib ];
 
@@ -7,10 +12,18 @@
       (
         let
           inherit (config.lib.iron-e.swayKey) lhs rhs;
+          inherit (config.wayland.windowManager.sway.config) terminal;
+          fd = lib.getExe pkgs.fd;
+          fish = lib.getExe config.programs.fish.package;
           fuzzel = lib.getExe config.programs.fuzzel.package;
         in
         {
           ${lhs.withMod lhs.space} = rhs.exec fuzzel;
+          ${lhs.withMod "x"} =
+            let
+              command = "${terminal} -e (${fd} -t x -L  . $PATH -x echo {/} 2>/dev/null | sort -iu | ${fuzzel} --dmenu | string escape || exit $status)";
+            in
+            rhs.exec "${fish} -c ${lib.escapeShellArg command}";
         }
       );
 }
