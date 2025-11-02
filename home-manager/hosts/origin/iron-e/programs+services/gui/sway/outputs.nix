f94a1b8bb9feaabc3ff62b0f2336861dbad8fc0d
Iron-E <code.iron.e@gmail.com>
2025-11-02 14:28:33 -0500

    feat(sway): set background options for monitors

--- home-manager/hosts/origin/iron-e/programs+services/gui/sway/outputs.nix
+++ home-manager/hosts/origin/iron-e/programs+services/gui/sway/outputs.nix
@@ -5,7 +5,8 @@
   home.packages = with pkgs; [ swaybg ];
   wayland.windowManager.sway.config =
     let
-      laptop = "AU Optronics 0x403D Unknown";
+      # HACK: cannot use identifier, see https://github.com/swaywm/swaybg/issues/44#issuecomment-1375655825
+      laptop = "eDP-1";
       leftMonitor = "LG Electronics LG HDR 4K 0x0004DF17";
       rightMonitor = "Dell Inc. DELL U2725QE 2B11H84";
     in
@@ -29,16 +30,19 @@
           mode = "2560x1440@59.951Hz";
           transform = "270";
           position = "0 0";
+          background = "${config.home.homeDirectory}/Pictures/wallpaper2.png fill #000000";
         };
 
         ${rightMonitor} = {
           mode = "2560x1440@59.951Hz";
           position = "1440 572";
+          background = "${config.home.homeDirectory}/Pictures/wallpaper.png fill #000000";
         };
 
         ${laptop} = {
           mode = "1920x1080@60.049Hz";
           position = "4000 572";
+          background = "${config.home.homeDirectory}/Pictures/wallpaper.png fill #000000";
         };
       };
 
