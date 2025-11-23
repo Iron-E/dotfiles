91f7542ff513c94d44a8e1e3d9f3dd9f3bfb6c72
Iron-E <code.iron.e@gmail.com>
2025-11-23 13:43:39 -0500

    fix(sway): title_window_icon command does not exist

--- home-manager/hosts/_extras_/programs+services/gui/wayland/extras/window-manager/sway/window.nix
+++ home-manager/hosts/_extras_/programs+services/gui/wayland/extras/window-manager/sway/window.nix
@@ -6,12 +6,5 @@
     border = 1;
     titlebar = false;
     hideEdgeBorders = "--i3 none";
-
-    commands = [
-      {
-        command = "title_window_icon padding 4px";
-        criteria.all = true;
-      }
-    ];
   };
 }
