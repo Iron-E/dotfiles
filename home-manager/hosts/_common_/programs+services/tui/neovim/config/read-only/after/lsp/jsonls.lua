b55c332f1a5e1e15f14706aa424df7451da71993
Iron-E <code.iron.e@gmail.com>
2025-12-17 18:47:50 -0500

    feat(neovim): support renovate config schema in jsonls

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/after/lsp/jsonls.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/after/lsp/jsonls.lua
@@ -4,6 +4,10 @@ return {
 		json = {
 			schemas = {
 				{
+					fileMatch = { "renovate.json", ".renovaterc", ".renovaterc.json" },
+					url = "https://docs.renovatebot.com/renovate-schema.json",
+				},
+				{
 					fileMatch = { "deno.json", "deno.jsonc" },
 					url = "https://raw.githubusercontent.com/denoland/deno/main/cli/schemas/config-file.v1.json",
 				},
