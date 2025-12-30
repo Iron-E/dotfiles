9a0b29463b0e980629ab27870e99b347ca10ef65
Iron-E <code.iron.e@gmail.com>
2025-12-30 18:03:31 -0500

    ref(git): do not fast forward merge by default

--- home-manager/hosts/_common_/programs+services/cli/git/config/merge.nix
+++ home-manager/hosts/_common_/programs+services/cli/git/config/merge.nix
@@ -3,7 +3,11 @@
   imports = [ ];
 
   programs.git.settings = {
-    merge.conflictstyle = "zdiff3";
+    merge = {
+      conflictstyle = "zdiff3";
+      ff = "no";
+    };
+
     rerere.enabled = true;
   };
 }
