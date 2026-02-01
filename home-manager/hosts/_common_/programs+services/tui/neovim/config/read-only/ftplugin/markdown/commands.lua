1768ef43180a7562c53c1eb11d4afd4ac023c23e
Iron-E <code.iron.e@gmail.com>
2026-02-01 12:14:55 -0500

    feat(neovim): set default prompt for :New based on current file name

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/ftplugin/markdown/commands.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/ftplugin/markdown/commands.lua
@@ -1,78 +1,59 @@
-local vertical_split = {
-	aboveleft = "left",
-	topleft = "left",
-	belowright = "right",
-	botright = "right",
-}
-
-local horizontal_split = {
-	aboveleft = "above",
-	topleft = "above",
-	belowright = "below",
-	botright = "below",
-}
-
----@param vertical vim.api.keyset.parse_cmd.mods
+---@param mods vim.api.keyset.parse_cmd.mods
 ---@return "above"|"left"|"right"|"below"
 local function get_split(mods)
 	if mods.vertical then
-		local resolved = vertical_split[mods.split]
-		if resolved ~= nil then
-			return resolved
+		if mods.split == "aboveleft" or mods.split == "topleft" then
+			return "left"
+		elseif mods.split == "belowright" or mods.split == "botright" then
+			return "right"
 		elseif vim.api.nvim_get_option_value("splitright", {}) then
 			return "right"
-		else
-			return "left"
 		end
+
+		return "left"
 	end
 
-	local resolved = horizontal_split[mods.split]
-	if resolved ~= nil then
-		return resolved
+	if mods.split == "aboveleft" or mods.split == "topleft" then
+		return "above"
+	elseif mods.split == "belowright" or mods.split == "botright" then
+		return "below"
 	elseif vim.api.nvim_get_option_value("splitbelow", {}) then
 		return "below"
-	else
-		return "above"
+	end
+
+	return "above"
+end
+
+--- @param bufnr integer
+--- @return string|nil title
+local function get_title(bufnr)
+	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, 100, false)
+	for _, line in ipairs(lines) do
+		local match = line:match("^# (.*)$")
+		if match ~= nil then
+			return match
+		end
 	end
 end
 
 --- @async
---- @param title? string
---- @param mods? vim.api.keyset.parse_cmd.mods
-local function create(title, mods)
-	if title == nil or title == "" then
-		vim.ui.input(
-			{ prompt = "Title" },
-			vim.schedule_wrap(function(text)
-				if type(text) == "string" and #text ~= 0 then
-					return create(text, mods)
-				end
-			end)
-		)
+--- @param from integer
+--- @param title string
+--- @param mods vim.api.keyset.parse_cmd.mods
+local function create(from, title, mods)
+	local from_file_path = vim.api.nvim_buf_get_name(from)
 
-		return
-	end
-
-	if mods == nil then
-		--- @diagnostic disable-next-line missing-fields
-		mods = {}
-	end
-
-	local split = get_split(mods)
-
-	local current_buf = vim.api.nvim_get_current_buf()
-	local current_file_path = vim.api.nvim_buf_get_name(current_buf)
-
-	local current_dir_name = vim.fs.dirname(current_file_path)
+	local from_dir_name = vim.fs.dirname(from_file_path)
 
 	local new_file_name = title:lower():gsub("%s", "-") .. ".md"
-	local new_file_path = vim.fs.joinpath(current_dir_name, new_file_name)
+	local new_file_path = vim.fs.joinpath(from_dir_name, new_file_name)
 
 	local new_buf = vim.api.nvim_create_buf(true, false)
 	vim.api.nvim_buf_set_name(new_buf, new_file_path)
 	vim.api.nvim_buf_set_lines(new_buf, 0, -1, true, { "# " .. title, "", "" })
 	vim.api.nvim_set_option_value("filetype", "markdown", { buf = new_buf })
 
+	local split = get_split(mods)
 	local win = vim.api.nvim_open_win(new_buf, true, {
 		split = split,
 		vertical = mods.vertical,
@@ -84,7 +65,23 @@ end
 vim.api.nvim_buf_create_user_command(0, "New", function(args)
 	--- @cast args vim.api.keyset.create_user_command.command_args
 
-	create(args.args, args.smods)
+	local current = vim.api.nvim_get_current_buf()
+
+	if args.args == nil or args.args == "" then
+		local default = get_title(current)
+
+		vim.ui.input({ prompt = "Title", default = default }, function(text)
+			if text == nil or #text == 0 then
+				return
+			end
+
+			create(current, text, args.smods)
+		end)
+
+		return
+	end
+
+	create(current, args.args, args.smods)
 end, {
 	desc = "Create a new markdown buffer with the given header.",
 	nargs = "?",
