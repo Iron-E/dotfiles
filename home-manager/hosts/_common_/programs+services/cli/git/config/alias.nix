04a8ab9ed6ff776afca30a33c1ea5417d51e2563
Iron-E <code.iron.e@gmail.com>
2026-02-09 18:10:44 -0500

    feat(git): ls aliases
    This is pretty useful metadata

--- home-manager/hosts/_common_/programs+services/cli/git/config/alias.nix
+++ home-manager/hosts/_common_/programs+services/cli/git/config/alias.nix
@@ -1,10 +1,42 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [ ];
 
   programs.git.settings.alias = {
     graph = "log --graph --pretty=format:'%C(#ffb7b7)%h%C(reset bold) %an%C(reset) %s %C(#ff4090)(%cr)%C(bold #ffa6ff)%d%C(reset)' --abbrev-commit --date=relative";
     main = "symbolic-ref refs/remotes/origin/HEAD --short";
+
+    ls = "ls-files";
+
+    # "list diff"
+    # list files that were changed in the given commit range.
+    # e.g. `g ls-diff HEAD`, or `g ls-diff HEAD~4..`
+    lsd =
+      lib.trim
+        # sh
+        ''
+          ! git diff-tree -r --no-commit-id --name-only "''${@:-HEAD}" #
+        '';
+
+    # "list log"
+    # list when files were last changed
+    lsl =
+      lib.trim # sh
+        ''
+          ! git ls-tree --abbrev=12 --format '%(objectname)%x09%(path)' "''${@:-HEAD}" #
+        '';
+
+    # "list log"
+    # list when files were last changed
+    lsla =
+      lib.trim # sh
+        ''
+          ! git lsl -tr "''${@:-HEAD}" #
+        '';
+
+    lsr = "ls-remote";
+
+    lst = "ls-tree";
 
     # "Print (Current) Branch"
     pb = "branch --show-current";
@@ -14,9 +46,10 @@
 
     pwd = "rev-parse --show-toplevel";
 
-    up = # sh
-      ''
-        ! git push --set-upstream origin "$(git pb)"
-      '';
+    up =
+      lib.trim # sh
+        ''
+          ! git push --set-upstream origin "$(git pb)"
+        '';
   };
 }
