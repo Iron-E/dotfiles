a129c70c611daaa0c3a3cc0777c1a84afd5f49dc
Iron-E <code.iron.e@gmail.com>
2026-02-09 17:37:58 -0500

    feat(git): alias to print remote tracking branch

--- home-manager/hosts/_common_/programs+services/cli/git/config/alias.nix
+++ home-manager/hosts/_common_/programs+services/cli/git/config/alias.nix
@@ -6,11 +6,11 @@
     graph = "log --graph --pretty=format:'%C(#ffb7b7)%h%C(reset bold) %an%C(reset) %s %C(#ff4090)(%cr)%C(bold #ffa6ff)%d%C(reset)' --abbrev-commit --date=relative";
     main = "symbolic-ref refs/remotes/origin/HEAD --short";
 
-    # "Print Current Branch"
-    pb = # sh
-      ''
-        ! git branch --show-current
-      '';
+    # "Print (Current) Branch"
+    pb = "branch --show-current";
+
+    # Print Origin Branch
+    opb = "rev-parse --abbrev-ref '@{upstream}'";
 
     pwd = "rev-parse --show-toplevel";
 
