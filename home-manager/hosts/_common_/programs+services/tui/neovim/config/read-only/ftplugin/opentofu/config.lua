2cea7ff0e708b12002c7ecd8909d10d3f6058925
Iron-E <code.iron.e@gmail.com>
2026-03-07 13:52:34 -0500

    chore(neovim): remove unnecessary imap for tf/tofu
    cindent fixes this

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/ftplugin/opentofu/config.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/ftplugin/opentofu/config.lua
@@ -1,4 +1,1 @@
 vim.api.nvim_set_option_value("commentstring", "# %s", { scope = "local" })
-
--- HACK: I should disable smartindent, but have to look into replacement more
-vim.api.nvim_buf_set_keymap(0, "i", "#", "<Space><BS>#", { noremap = true })
