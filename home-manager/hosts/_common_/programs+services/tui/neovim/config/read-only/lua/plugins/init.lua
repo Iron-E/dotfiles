81919d8f9bb77c4ce381312d885edecfedf15662
Iron-E <code.iron.e@gmail.com>
2026-04-03 23:08:59 -0400

    feat(neovim): Pack* cmds

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/init.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/init.lua
@@ -131,3 +131,55 @@ vim.pack.add({
 -- require("plugins.lazy")
 require("plugins.undotree")
 require("stenvim"):register()
+
+--- @return string[]
+local function get_package_names()
+	local plugins = vim.pack.get(nil, { info = false })
+
+	return vim.iter(ipairs(plugins))
+		:map(function(_, plugin)
+			return plugin.spec.name
+		end)
+		:totable()
+end
+
+vim.api.nvim_create_user_command("PackClean", function()
+	local plugins = vim.pack.get(nil, { info = false })
+
+	local active_plugin_names = {}
+	for _, plugin in ipairs(plugins) do
+		if not plugin.active then
+			table.insert(active_plugin_names, plugin.spec.name)
+		end
+	end
+
+	vim.pack.del(active_plugin_names)
+end, {
+	desc = "Clean inactive packages",
+})
+
+vim.api.nvim_create_user_command("PackRestore", function(args)
+	local fargs
+	if #args.fargs > 0 then
+		fargs = args.fargs
+	end
+
+	vim.pack.update(fargs, { target = "lockfile" })
+end, {
+	desc = "Restore a package to its lockfile version",
+	nargs = "?",
+	complete = get_package_names,
+})
+
+vim.api.nvim_create_user_command("PackUpdate", function(args)
+	local fargs
+	if #args.fargs > 0 then
+		fargs = args.fargs
+	end
+
+	vim.pack.update(fargs)
+end, {
+	desc = "Clean inactive packages",
+	nargs = "?",
+	complete = get_package_names,
+})
