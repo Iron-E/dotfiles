e705ae471db926266d6c280a24a920268226bdb3
Iron-E <code.iron.e@gmail.com>
2026-04-07 09:55:27 -0400

    ref(neovim): separate session write/read/delete commands
    The native autocompletion is better IMO.

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/pack/mini/sessions/init.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/pack/mini/sessions/init.lua
@@ -1,3 +1,5 @@
+local local_session_name = ".session.vim"
+
 require("mini.sessions").setup({
 	-- Whether to read default session if Neovim opened without file arguments
 	autoread = false,
@@ -9,7 +11,7 @@ require("mini.sessions").setup({
 	directory = vim.fn.stdpath("data") .. "/mini.sessions",
 
 	-- File for local session (use `''` to disable)
-	file = ".session.vim",
+	file = local_session_name,
 
 	-- Whether to force possibly harmful actions (meaning depends on function)
 	force = {
@@ -37,23 +39,56 @@ vim.api.nvim_create_user_command("Restart", "lua MiniSessions.restart()", {
 	desc = "Restart neovim and preserve current session.",
 })
 
-vim.api.nvim_create_user_command("Session", function(args)
-	--- @type nil|string
-	local action
-	if args.args ~= "" then
-		action = args.args
+--- @param name string
+--- @return nil|string
+local function get_input_session_name(name)
+	if name == "" then
+		return
 	end
 
-	MiniSessions.select(action)
+	return name
+end
+
+--- @return string[]
+local function get_session_names()
+	local session_names = {}
+	for _, v in pairs(MiniSessions.detected) do
+		table.insert(session_names, v.name)
+	end
+
+	return session_names
+end
+
+vim.api.nvim_create_user_command("SessionWrite", function(args)
+	MiniSessions.write(get_input_session_name(args.args))
 end, {
-	desc = "Manage sessions via mini.sessions",
+	desc = "Write sessions via mini.sessions",
 	nargs = "?",
 	complete = function()
-		return { "read", "delete", "write" }
+		local session_names = get_session_names()
+
+		table.insert(session_names, 1, local_session_name)
+		return vim.list.unique(session_names)
 	end,
 })
 
-vim.api.nvim_set_keymap("n", "<A-s>", ":<C-u>Session ", {
+vim.api.nvim_create_user_command("SessionRead", function(args)
+	MiniSessions.read(get_input_session_name(args.args))
+end, {
+	desc = "Read sessions via mini.sessions",
+	nargs = "?",
+	complete = get_session_names,
+})
+
+vim.api.nvim_create_user_command("SessionDelete", function(args)
+	MiniSessions.delete(get_input_session_name(args.args))
+end, {
+	desc = "Delete sessions via mini.sessions",
+	nargs = "?",
+	complete = get_session_names,
+})
+
+vim.api.nvim_set_keymap("n", "<A-s>", ":<C-u>Session", {
 	desc = "Manage sessions via mini.sessions",
 	noremap = true,
 })
