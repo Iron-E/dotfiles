ad345309dc89cf9132c11a59a07b69f465554c4d
Iron-E <code.iron.e@gmail.com>
2026-04-10 21:53:27 -0400

    fix(neovim): only enable nvim-highlight-colors when document_color not enabled

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/pack/highlight-colors/init.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/pack/highlight-colors/init.lua
@@ -6,4 +6,9 @@ require("nvim-highlight-colors").setup({
 	enable_named_colors = true,
 	enable_tailwind = true,
 	render = "virtual",
+
+	exclude_buffer = function(bufnr)
+		-- TODO: should this be for all the clients on a buffer?
+		return vim.lsp.document_color.is_enabled({ bufnr = bufnr })
+	end,
 })
