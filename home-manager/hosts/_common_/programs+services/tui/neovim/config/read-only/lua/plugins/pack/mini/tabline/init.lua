d67db08f3f4d8670484a0411e6527834471b8a0b
Iron-E <code.iron.e@gmail.com>
2026-04-13 18:11:40 -0400

    fix(neovim): tabline sometimes not correctly shown/hidden

--- home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/pack/mini/tabline/init.lua
+++ home-manager/hosts/_common_/programs+services/tui/neovim/config/read-only/lua/plugins/pack/mini/tabline/init.lua
@@ -1,28 +1,33 @@
 require("mini.tabline").setup()
 
-local function buf_is_listed(bufnr)
-	return vim.api.nvim_buf_is_valid(bufnr) and vim.api.nvim_get_option_value("buflisted", { buf = bufnr })
-end
-
 -- don't show my statusline unless is is because of the autocmd below
 vim.api.nvim_set_option_value("showtabline", 0, {})
 
--- TODO: this can probably be separated out into create and destroy events.
---       create events don't need to check for hiding the tabline, destroy events don't need to check
---       for showing it?
+local function at_least_two_listed_buffers()
+	local count = 0
+	for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.api.nvim_buf_is_valid(bufnr) and vim.api.nvim_get_option_value("buflisted", { buf = bufnr }) then
+			count = count + 1
+
+			if count > 1 then
+				return true
+			end
+		end
+	end
+
+	return false
+end
+
 local group = vim.api.nvim_create_augroup("config.mini.tabline", { clear = true })
 vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete", "TabClosed", "TabNew", "VimEnter" }, {
 	desc = "Hide tabline when only one buffer open",
 	group = group,
-	callback = function()
-		local value = 0
-		if
-			#vim.api.nvim_list_tabpages() > 1
-			or #vim.iter(ipairs(vim.api.nvim_list_bufs())):filter(buf_is_listed):totable() > 1
-		then
-			value = 2
+	callback = vim.schedule_wrap(function()
+		local showtabline = 0
+		if vim.fn.tabpagenr("$") > 1 or at_least_two_listed_buffers() then
+			showtabline = 2
 		end
 
-		vim.api.nvim_set_option_value("showtabline", value, {})
-	end,
+		vim.api.nvim_set_option_value("showtabline", showtabline, {})
+	end),
 })
