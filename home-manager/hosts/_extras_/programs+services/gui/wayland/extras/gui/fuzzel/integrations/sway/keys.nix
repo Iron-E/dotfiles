52704ae89c9de5c29a37c91d7a951ba118702ecf
Iron-E <code.iron.e@gmail.com>
2026-05-02 15:26:44 -0400

    fix(fuzzel): running arbitrary command in terminal

--- home-manager/hosts/_extras_/programs+services/gui/wayland/extras/gui/fuzzel/integrations/sway/keys.nix
+++ home-manager/hosts/_extras_/programs+services/gui/wayland/extras/gui/fuzzel/integrations/sway/keys.nix
@@ -12,18 +12,30 @@
       (
         let
           inherit (config.lib.iron-e.swayKey) lhs rhs;
-          inherit (config.wayland.windowManager.sway.config) terminal;
-          fd = lib.getExe pkgs.fd;
           fish = lib.getExe config.programs.fish.package;
           fuzzel = lib.getExe config.programs.fuzzel.package;
         in
         {
           ${lhs.withMod lhs.space} = rhs.exec fuzzel;
-          ${lhs.withMod "x"} =
-            let
-              command = "${terminal} -e (${fd} -t x -L  . $PATH -x echo {/} 2>/dev/null | sort -iu | ${fuzzel} --dmenu | string escape || exit $status)";
-            in
-            rhs.exec "${fish} -c ${lib.escapeShellArg command}";
+          ${lhs.withMod "x"} = rhs.exec "${fish} -c run_fuzzel_command";
+        }
+      );
+
+  programs.fish.functions.run_fuzzel_command =
+    lib.optionalAttrs config.wayland.windowManager.sway.enable
+      (
+        let
+          inherit (config.wayland.windowManager.sway.config) terminal;
+          fd = lib.getExe pkgs.fd;
+          fuzzel = lib.getExe config.programs.fuzzel.package;
+        in
+        {
+          description = "Run a command selected by a fuzzel prompt.";
+          body = # fish
+            ''
+              ${fd} -t x -L . $PATH -x echo {/} 2>/dev/null | sort -iu | ${fuzzel} --dmenu | read -ta cmd
+              ${terminal} -e $cmd
+            '';
         }
       );
 }
