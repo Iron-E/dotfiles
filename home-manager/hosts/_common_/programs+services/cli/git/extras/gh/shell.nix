bbdab5f7fe4cdec39661ecd335dbefc453b8ce9b
Iron-E <code.iron.e@gmail.com>
2026-05-30 19:02:30 -0400

    ref(gh): alias to ghub

--- home-manager/hosts/_common_/programs+services/cli/git/extras/gh/shell.nix
+++ home-manager/hosts/_common_/programs+services/cli/git/extras/gh/shell.nix
@@ -3,5 +3,5 @@
   imports = [ ];
 
   # because `gh` is short for `git show`
-  home.shellAliases.github = lib.getExe config.programs.gh.package;
+  home.shellAliases.ghub = lib.getExe config.programs.gh.package;
 }
