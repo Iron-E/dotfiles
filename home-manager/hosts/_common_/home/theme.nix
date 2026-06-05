471207ea4e72d4a6b8441aeb6235c416a686b521
Iron-E <code.iron.e@gmail.com>
2026-06-05 18:54:45 -0400

    fix(home): only set theme on linux

--- home-manager/hosts/_common_/home/theme.nix
+++ home-manager/hosts/_common_/home/theme.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   imports = [ ];
 
-  home.pointerCursor = {
+  home.pointerCursor = lib.optionalAttrs pkgs.stdenv.isLinux {
     name = "Bibata-Modern-Classic";
     package = pkgs.bibata-cursors;
   };
