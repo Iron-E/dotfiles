5c2df989ee5b00ecede4a0c48cc5f76a249ea6cd
Iron-E <code.iron.e@gmail.com>
2026-06-05 19:55:56 -0400

    feat: migrate to 26.05

--- home-manager/hosts/_common_/home/config.nix
+++ home-manager/hosts/_common_/home/config.nix
@@ -7,5 +7,13 @@
     homeDirectory = "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/${config.home.username}";
     language.base = "en_US.UTF-8";
     preferXdgDirectories = true;
+
+    extraOutputsToInstall = [
+      "devdoc"
+      "devman"
+      "doc"
+      "info"
+      "man"
+    ];
   };
 }
