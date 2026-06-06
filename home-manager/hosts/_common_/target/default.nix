2c8f01061d43fe89ac8e807ab47b97e906d404ed
Iron-E <code.iron.e@gmail.com>
2026-06-05 23:03:41 -0400

    feat(target): configure darwin defaults

--- home-manager/hosts/_common_/target/default.nix
+++ home-manager/hosts/_common_/target/default.nix
@@ -21,6 +21,19 @@ in
     darwin = lib.optionalAttrs pkgs.stdenv.isDarwin {
       copyApps.enable = false;
       linkApps.enable = true;
+      search = "DuckDuckGo";
+      defaults.NSGlobalDomain = {
+        AppleShowAllExtensions = true;
+        AppleICUForce24HourTime = true;
+        AppleInterfaceStyle = "Dark";
+        AppleScrollerPagingBehavior = true;
+        AppleShowAllFiles = true;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSDocumentSaveNewDocumentsToCloud = false;
+        NSWindowShouldDragOnGesture = true;
+        "com.apple.keyboard.fnState" = true;
+      };
     };
   };
 }
