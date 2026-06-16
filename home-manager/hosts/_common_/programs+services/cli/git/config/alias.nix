5006ccc4135f165f2bdd2d6863ccc2618f6bf461
Iron-E <code.iron.e@gmail.com>
2026-06-16 08:17:32 -0400

    feat(git): ls-* helpers

--- home-manager/hosts/_common_/programs+services/cli/git/config/alias.nix
+++ home-manager/hosts/_common_/programs+services/cli/git/config/alias.nix
@@ -3,28 +3,40 @@
   imports = [ ];
 
   programs.git.settings.alias = {
+    ###############
+    # LOG HELPERS #
+    ###############
+
     graph = "log --graph --pretty=format:'%C(#ffb7b7)%h%C(reset bold) %an%C(reset) %s %C(#ff4090)(%cr)%C(bold #ffa6ff)%d%C(reset)' --abbrev-commit --date=relative";
-    main = "symbolic-ref refs/remotes/origin/HEAD --short";
 
     clog = "log --topo-order --oneline --cherry";
 
     tlog = "log --topo-order";
 
     ls = "ls-files";
+    ls-added = "diff --name-only --diff-filter=A";
+    ls-broken = "diff --name-only --diff-filter=B";
+    ls-copied = "diff --name-only --diff-filter=C";
+    ls-deleted = "diff --name-only --diff-filter=D";
+    ls-modified = "diff --name-only --diff-filter=M";
+    ls-renamed = "diff --name-only --diff-filter=R";
+    ls-type-changed = "diff --name-only --diff-filter=T";
+    ls-unknown = "diff --name-only --diff-filter=X";
+    ls-unmerged = "diff --name-only --diff-filter=U";
 
     # "list diff"
     # list files that were changed in the given commit range.
     # e.g. `g ls-diff HEAD`, or `g ls-diff HEAD~4..`
-    lsd =
+    ls-diff =
       lib.trim
         # sh
         ''
-          ! git diff-tree -r --no-commit-id --name-only "''${@:-HEAD}" #
+          ! git diff --name-only "''${@:-HEAD}" #
         '';
 
     # "list log"
     # list when files were last changed
-    lsl =
+    ls-log =
       lib.trim # sh
         ''
           ! git ls-tree --abbrev=12 --format '%(objectname)%x09%(path)' "''${@:-HEAD}" #
@@ -32,18 +44,28 @@
 
     # "list log"
     # list when files were last changed
-    lsla =
+    ls-log-recurse =
       lib.trim # sh
         ''
-          ! git lsl -tr "''${@:-HEAD}" #
+          ! git ls-log -tr "''${@:-HEAD}" #
         '';
 
     lsr = "ls-remote";
 
     lst = "ls-tree";
 
+    ################
+    # PATH HELPERS #
+    ################
+
     # print working directory
     pwd = "rev-parse --show-toplevel";
+
+    ##################
+    # BRANCH ALIASES #
+    ##################
+
+    main = "symbolic-ref refs/remotes/origin/HEAD --short";
 
     # "Print (Current) Branch"
     pb = "branch --show-current";
@@ -59,6 +81,10 @@
 
     # Print Upstream Branch
     upb = "rev-parse --abbrev-ref '@{upstream}'";
+
+    ##################
+    # BRANCH HELPERS #
+    ##################
 
     up =
       lib.trim # sh
