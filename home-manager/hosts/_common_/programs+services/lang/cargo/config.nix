5a89757a4d46fb7625307bcecf9e264b247743b2
Iron-E <code.iron.e@gmail.com>
2026-07-01 13:55:06 -0400

    feat(cargo): use sccache for builds

--- home-manager/hosts/_common_/programs+services/lang/cargo/config.nix
+++ home-manager/hosts/_common_/programs+services/lang/cargo/config.nix
@@ -1,13 +1,13 @@
 {
   pkgs,
   lib,
-  config,
   ...
 }:
 {
   imports = [ ];
 
   home.file.".cargo/config.toml".source = (pkgs.formats.toml { }).generate "cargo-config" {
+    build.rustc-wrapper = lib.getExe pkgs.sccache;
     target.x86_64-unknown-linux-gnu = {
       linker = "clang";
       rustflags = [
