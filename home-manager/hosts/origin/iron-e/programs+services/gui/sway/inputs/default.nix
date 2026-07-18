bf82a58d182283d847c9396bb21f248331df5bcc
Iron-E <code.iron.e@gmail.com>
2026-07-18 15:31:37 -0400

    feat(sway): configure old keyboard

--- home-manager/hosts/origin/iron-e/programs+services/gui/sway/inputs/default.nix
+++ home-manager/hosts/origin/iron-e/programs+services/gui/sway/inputs/default.nix
@@ -5,6 +5,34 @@ in
 {
   imports = util.fs.readSubmodules ./.;
 
+  # From: https://www.reddit.com/r/swaywm/comments/156wsoe/comment/jt3id0x/
+  #
+  #> It should be possible to do this using a custom keymap. See if the following works:
+  #>
+  #> Create a my_layout.xkb somewhere.
+  #>
+  #> ```
+  #> xkb_keymap {
+  #>     xkb_keycodes { include "evdev+aliases(qwerty)" };
+  #>     xkb_types { include "complete" };
+  #>     xkb_compat { include "complete" };
+  #>     xkb_symbols {
+  #>         include "pc"
+  #>         include "us"
+  #>         include "inet(evdev)"
+  #>         replace key <RALT> { [ Super_R ] };
+  #>     };
+  #> };
+  #> ```
+  #>
+  #> And add in your sway config:
+  #>
+  #> ```
+  #> input type:keyboard {
+  #>     xkb_file "PATH/TO/my_layout.xkb"
+  #> }
+  #> ```
+
   wayland.windowManager.sway.extraConfig = # i3config
     ''
       input "9639:64097:Compx_2.4G_Receiver_Mouse" {
@@ -26,6 +54,10 @@ in
 
       input "1:1:AT_Translated_Set_2_keyboard" {
         xkb_file "${./builtin.xkb}"
+      }
+
+      input "1266:6192:DELL_Alienware_510K" {
+        xkb_file "${./normal-external.xkb}"
       }
     '';
 }
